ebfa5ab149a4690a324283573659b45e41f70329
Martin Lehmann <lehmar11@student.westerdals.no>
2015-01-25 19:56:04 +0100

    Possible to delete projects.

--- server-node/controllers/projects.js
+++ server-node/controllers/projects.js
@@ -57,12 +57,50 @@ router.get('/:projectId', function (req, res, next) {
     Project.findOne({_id: projectId}, function (err, project) {
         if (err) return next(err);
 
+        if (!project) {
+            return res.status(404).send('No project found with id  \'' + projectId + '\'');
+        }
+
         if (!_.contains(project.members, jwtUser.username)) {
             return res.status(401).send('The user \'' + jwtUser.username + '\' does not have access to this project');
         }
 
         return res.status(200).json(project);
     });
+});
+
+router.delete('/:projectId', function(req, res, next) {
+    var token = req.headers['x-auth'];
+
+    if (!token) {
+        return res.status(401).send('No token supplied');
+    }
+
+    var jwtUser = jwt.decode(token, global.jwtSecret);
+
+    var projectId = req.params.projectId;
+
+    Project.findOne({_id: projectId}, function (err, project) {
+        if (err) return next(err);
+
+        if (!project) {
+            return res.status(404).send('No project found with id  \'' + projectId + '\'');
+        }
+
+        if (!_.contains(project.members, jwtUser.username)) {
+            return res.status(401).send('The user \'' + jwtUser.username + '\' does not have access to this project');
+        }
+
+        // TODO: Just archive the project, don't actually delete it(?)
+        Project.remove({_id: projectId}, function () {
+            return res.status(204).send();
+        });
+    });
+});
+
+router.get('/:projectId/issues', function(req, res) {
+    var projectId = req.params.projectId;
+    return res.send('hi ' + projectId);
 });
 
 router.post('/:projectId/issues/', function (req, res, next) {
